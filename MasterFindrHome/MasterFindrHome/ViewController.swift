866ef693fac615bc01bc93cf9f2a84878ffb0aca
Georgie Halpern <andreahalpern@Andreas-MacBook-Pro-3.local>
2015-06-05 22:02:25 -0400

    should be working 6/5

--- MasterFindrHome/MasterFindrHome/ViewController.swift
+++ MasterFindrHome/MasterFindrHome/ViewController.swift
@@ -9,24 +9,11 @@ import UIKit
 import MapKit
 import GoogleMaps
 
-class MapViewController: UIViewController, MKMapViewDelegate {
-    
-    
-    @IBOutlet weak var mapView: MKMapView!
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var camera = GMSCameraPosition.cameraWithLatitude(41.8262, longitude: -71.4032, zoom: 12)
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        var demoMarker = GMSMarker()
-        demoMarker.position = camera.target
-        demoMarker.snippet = "I lost my Canada Goose jacket!"
-        demoMarker.appearAnimation = kGMSMarkerAnimationPop
-        demoMarker.map = mapView
-        self.view = mapView
-        // Do any additional setup after loading the view, typically from a nib.
     }
-    
     
 }
 

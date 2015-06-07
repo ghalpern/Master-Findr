3d70d57663f00d28b7e2eb412984d79f9d3e5925
Eli Motycka <elimotycka@Eli-Motyckas-MacBook-Pro.local>
2015-06-06 23:52:05 -0400

    pins now work but camera doesn't open on Providence.

--- MasterFindrHome/MasterFindrHome/GoogleMapsViewController.swift
+++ MasterFindrHome/MasterFindrHome/GoogleMapsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import GoogleMaps
 
-class GoogleMapsViewController: UIViewController {
+class GoogleMapsViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
@@ -19,23 +19,21 @@ class GoogleMapsViewController: UIViewController {
         super.viewDidLoad()
         var camera = GMSCameraPosition.cameraWithLatitude(41.8262, longitude: -71.4032, zoom: 12)
         var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        var demoMarker = GMSMarker()
-        demoMarker.position = camera.target
-        demoMarker.snippet = "I lost my Canada Goose jacket! :("
-        demoMarker.appearAnimation = kGMSMarkerAnimationPop
-        demoMarker.map = mapView
-        
-        self.view = mapView
+        self.mapView.delegate = self
         
         // Do any additional setup after loading the view, typically from a nib.
-        
+
     }
     
     
     @IBAction func addPin(sender: UILongPressGestureRecognizer) {
-        
         if sender.state == UIGestureRecognizerState.Began {
             let coordinate = mapView.convertPoint(sender.locationInView(mapView), toCoordinateFromView: mapView)
+            let ItemLocationPin = MKPointAnnotation()
+            ItemLocationPin.coordinate.latitude = coordinate.latitude
+            ItemLocationPin.coordinate.longitude = coordinate.longitude
+            mapView.addAnnotation(ItemLocationPin)
+            
             println("Coordinates: \(coordinate.latitude, coordinate.longitude)")
        }
     }

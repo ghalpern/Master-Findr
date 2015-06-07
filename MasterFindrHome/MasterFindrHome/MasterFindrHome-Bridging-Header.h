8f8ef9c34abd8fb2c45c9b86528c23cbed26653e
Eli Motycka <elimotycka@Eli-Motyckas-MacBook-Pro.local>
2015-06-07 19:05:38 -0400

    hahahaha

---
+++ MasterFindrHome/MasterFindrHome/MasterFindrHome-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MasterFindrHome-Bridging-Header.h
+//  MasterFindrHome
+//
+//  Created by Georgie Halpern on 6/6/15.
+//  Copyright (c) 2015 ghalpern. All rights reserved.
+//
+#import "KeychainWrapper.h"
+#ifndef MasterFindrHome_MasterFindrHome_Bridging_Header_h
+#define MasterFindrHome_MasterFindrHome_Bridging_Header_h
+
+
+#endif
